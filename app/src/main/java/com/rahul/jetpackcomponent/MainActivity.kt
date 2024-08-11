a14e45c2e6ef9a562e9dc42676869b19143e1230
Rahul Zala <zalarahul000!gmail.com>
2024-08-11 13:09:11 +0530

    add life cycle aware component observer

--- app/src/main/java/com/rahul/jetpackcomponent/MainActivity.kt
+++ app/src/main/java/com/rahul/jetpackcomponent/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.rahul.jetpackcomponent
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Activity", "onStart")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        lifecycle.addObserver(Observer())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Activity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Activity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Activity", "onStop")
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Activity", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Activity", "onRestart")
     }
 }
