666064612cc8573e61e2c9b898389d9763ab7a49
Rahul Zala <zalarahul000!gmail.com>
2024-08-11 13:20:51 +0530

    state changed and data loss

--- app/src/main/java/com/rahul/jetpackcomponent/MainActivity.kt
+++ app/src/main/java/com/rahul/jetpackcomponent/MainActivity.kt
@@ -3,42 +3,24 @@ package com.rahul.jetpackcomponent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.rahul.jetpackcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("Activity", "onStart")
-    }
-
+    lateinit var binding: ActivityMainBinding
+    var count=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        lifecycle.addObserver(Observer())
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnadd.setOnClickListener {
+            increment()
+        }
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("Activity", "onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("Activity", "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("Activity", "onStop")
-
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("Activity", "onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("Activity", "onRestart")
+    private fun increment(){
+        count++
+        binding.tvCounter.text = count.toString()
     }
 }
